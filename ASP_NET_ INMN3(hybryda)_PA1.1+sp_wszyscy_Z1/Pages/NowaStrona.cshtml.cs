955e7d8d086f713c0dd6d1647f6c3fc115fb224c
PMBytner <pawel.m.bytner@gmail.com>
2023-09-30 13:04:43 +0200

    Dodaj pliki projektów.

---
+++ ASP_NET_ INMN3(hybryda)_PA1.1+sp_wszyscy_Z1/Pages/NowaStrona.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET__INMN3_hybryda__PA1._1_sp_wszyscy_Z1.Pages
+{
+    public class NowaStronaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
